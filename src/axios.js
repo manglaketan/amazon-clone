26292d8311db350cee98d752f6912c694a82b792
Ketan Mangla <manglaketan08@gmail.com>
2020-09-15 02:06:03 +0530

    added deployed url to axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:5001/clone-a8a3b/us-central1/api", //this is where we need to have the API URL or cloud fucntion URL
+	baseURL: "https://morning-retreat-13727.herokuapp.com/",
+	// "http://localhost:5001/clone-a8a3b/us-central1/api", //this is where we need to have the API URL or cloud fucntion URL
 });
 
 export default instance;
